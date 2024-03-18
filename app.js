e9ae56dd92b6b0f29f8a9c705608a8c0e133fdf7
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-19 01:12:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let quote = "A person who never made a mistke never tried anything new";
+let famous_person = "Albert Einstein";
+let message = `${famous_person} once said, ${qoute}`;
+console.log(message);
---
+++ app.ts
@@ -0,0 +1,4 @@
+let quote: string= "A person who never made a mistke never tried anything new";
+let famous_person="Albert Einstein";
+let message=`${famous_person} once said, ${qoute}`;
+console.log(message); 
